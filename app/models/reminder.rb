993be9d66642dc873207ec2cc5de295cfcc1552c
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-09-03 15:13:22 -0400

    added support for days, weeks, and years

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -4,7 +4,18 @@ class Reminder < ActiveRecord::Base
 	def create_reminder_items
 		doses.times do |n|
 			ri = ReminderItem.new
-			ri.scheduled_time = n.days.from_now
+			
+			case time_period.downcase
+			when "day"
+				ri.scheduled_time = (n.to_f/num_per).days.from_now
+			when "week"
+				ri.scheduled_time = (n.to_f/num_per).weeks.from_now
+			when "year"
+				ri.scheduled_time = (n.to_f/num_per*365).days.from_now
+			else
+				return false
+			end
+
 			ri.reminder = self
 			ri.save
 		end
