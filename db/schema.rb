e813ca219febe7651395698dc6265490c2ce4f79
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-09-03 15:39:25 -0400

    removed home scaffolding, changed to a controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903191545) do
+ActiveRecord::Schema.define(version: 20140903192725) do
+
+  create_table "homes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "reminder_items", force: true do |t|
     t.boolean  "taken"
