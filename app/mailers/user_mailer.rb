99221ad380889a316f7c9257f4f1d0c5712ffb93
Samuel Zobel <samuelezobel@gmail.com>
2014-09-05 19:31:47 -0400

    got mail to work and send in dev mode

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,24 @@
 class UserMailer < ActionMailer::Base
-  default from: "samuelezobel@gmail.com"
-end
+  default from: "ortiz_frank@hotmail.com"
 
-def welcome_email(user)
-    @user = user
-    @url  = 'https://www.mdlive.com/consumer/choose.html'
-    mail(to: @user.email, subject: 'You have a Reminder!')
-  end
+
+	def welcome_email(user)
+		@user = user
+	    # @url  = 'https://www.mdlive.com/consumer/choose.html'
+	    mail(to: user.email, subject: 'You have a Reminder!')
+	end
+
+
+
+# attachments['filename.jpg'] = File.read('/path/to/filename.jpg')
+
+# encoded_content = SpecialEncode(File.read('/path/to/filename.jpg'))
+# attachments['filename.jpg'] = {mime_type: 'application/x-gzip',
+# 	encoding: 'SpecialEncoding',
+# 	content: encoded_content }
+
+	# def welcome
+	# 	attachments.inline['image.jpg'] = File.read('/path/to/image.jpg')
+	# end
+
 end
