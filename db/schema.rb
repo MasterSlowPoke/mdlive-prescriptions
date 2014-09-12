6b0133ef2b74820d4c896a10f3b5f22c2e2f775f
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-09-12 14:33:33 -0400

    fixed missing ends from Sam's commit in lib/tasks/reminder.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140909195158) do
-
+ActiveRecord::Schema.define(version: 20140909225014) do
 
   create_table "homes", force: true do |t|
     t.datetime "created_at"
