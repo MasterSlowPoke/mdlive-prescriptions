7c927454f58a5bf52f005d7c465674b6e51dcdc2
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-09-15 11:09:20 -0400

    generated migration to remove Reminder.num_per

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915010158) do
+ActiveRecord::Schema.define(version: 20140915150802) do
 
   create_table "homes", force: true do |t|
     t.datetime "created_at"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20140915010158) do
 
   create_table "reminders", force: true do |t|
     t.string   "title"
-    t.integer  "num_per"
     t.string   "time_period"
     t.text     "notes"
     t.datetime "start"
