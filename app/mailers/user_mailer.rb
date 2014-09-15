2788891cbaf6562622047d39517614dd37b95150
Samuel Zobel <samuelezobel@gmail.com>
2014-09-15 14:21:40 -0400

    updating uncommited changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,14 +4,15 @@ class UserMailer < ActionMailer::Base
 
 	def welcome_email(user)
 		@user = user
-		Rails.logger.info "sending welcome email to #{user.email}"
+
 	    # @url  = 'https://www.mdlive.com/consumer/choose.html'
 	    mail(to: user.email, subject: 'Welcome!')
 	end
 
 
-	def reminder_email(user)
+	def reminder_email(user, reminder)
 		@user = user
+		@reminder = reminder
 		mail(to: user.email, subject: 'You have a Reminder!')
 	end
 
--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,6 +1,8 @@
 class Reminder < ActiveRecord::Base
 	has_many :reminder_items
 
+	after_create :send_new_reminder_email
+
 	def set_first_dose(date, time)
 		self.start = DateTime.strptime(date + " | " + time, "%Y-%m-%d | %H:%M")
 	end
@@ -69,24 +71,8 @@ class Reminder < ActiveRecord::Base
 		all_doses.sort
 	end
 
-
-	# def create_reminder_items
-	# 	doses.times do |n|
-	# 		ri = ReminderItem.new
-			
-	# 		case time_period.downcase
-	# 		when "day"
-	# 			ri.scheduled_time = (n.to_f/num_per).days.from_now
-	# 		when "week"
-	# 			ri.scheduled_time = (n.to_f/num_per).weeks.from_now
-	# 		when "year"
-	# 			ri.scheduled_time = (n.to_f/num_per*365).days.from_now
-	# 		else
-	# 			return false
-	# 		end
-
-	# 		ri.reminder = self
-	# 		ri.save
-	# 	end
-	# end
+protected
+def send_new_reminder_email
+	UserMailer.reminder_email(user, self).deliver
 end
+end 
