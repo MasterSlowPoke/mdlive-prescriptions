dc9cd9422c798c6a0e88b447d580897281ec9aaa
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-09-20 18:51:35 -0400

    migrated Users to have an boolean Admin column, default false

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140918214729) do
+ActiveRecord::Schema.define(version: 20140920224947) do
 
   create_table "reminder_rules", force: true do |t|
     t.integer  "reminder_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "day_of_week", limit: 255
+    t.string   "day_of_week"
     t.string   "time_of_day"
     t.text     "schedule"
   end
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20140918214729) do
   add_index "schedule_logs", ["name"], name: "index_schedule_logs_on_name"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140918214729) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
