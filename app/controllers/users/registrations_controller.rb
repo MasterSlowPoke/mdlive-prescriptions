219f9b9bd8da519e6b31f322acc337151bd61d2d
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-13 20:48:04 -0400

    removed surplus email function

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,4 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-	def create
-		super do |user|
-			next unless user.persisted?
-			UserMailer.welcome_email(user).deliver
-		end
-	end
-
 	def sign_up_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
