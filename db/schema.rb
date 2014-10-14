a543d169312774dfdf5b3234bf80277a0bba4c8b
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-13 23:39:52 -0400

    migration to change user.phone to an integer with at least 10 digits

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012183624) do
+ActiveRecord::Schema.define(version: 20141014020907) do
 
   create_table "reminder_rules", force: true do |t|
     t.integer  "reminder_id"
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20141012183624) do
   add_index "schedule_logs", ["name"], name: "index_schedule_logs_on_name"
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                            default: "",    null: false
+    t.string   "encrypted_password",               default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                    default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -58,8 +58,8 @@ ActiveRecord::Schema.define(version: 20141012183624) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
-    t.boolean  "admin",                  default: false
-    t.string   "phone"
+    t.boolean  "admin",                            default: false
+    t.integer  "phone",                  limit: 8
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
