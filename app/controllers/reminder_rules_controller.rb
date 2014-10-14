be07e92aaa35d3c3b4a35ee8810448e8d002f531
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-14 12:31:14 -0400

    reminder rules redirect to the parent reminder when they can't be saved

--- app/controllers/reminder_rules_controller.rb
+++ app/controllers/reminder_rules_controller.rb
@@ -25,12 +25,14 @@ class ReminderRulesController < ApplicationController
   # POST /reminder_rules
   # POST /reminder_rules.json
   def create
+    @reminder_rule = ReminderRule.new(reminder_rule_params)
+
     respond_to do |format|
-      if @reminder_rule = ReminderRule.new(reminder_rule_params)
+      if @reminder_rule.save
         format.html { redirect_to @reminder_rule.reminder, notice: 'Reminder rule was successfully created.' }
         format.json { render :show, status: :created, location: @reminder_rule }
       else
-        format.html { render :new }
+        format.html { redirect_to @reminder_rule.reminder, alert: 'Reminder Time is invalid.' }
         format.json { render json: @reminder_rule.errors, status: :unprocessable_entity }
       end
     end
