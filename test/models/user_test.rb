7b954f776a9620442b55c1d8ed082f40c4282ea3
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-17 23:45:14 -0400

    implemented tests for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do 
+    @user = users(:one)
+    @user.password = "password"
+    assert @user.save, "user object not savable"
+  end
+
+  test "user name never nil" do
+    @user.name = nil
+    refute @user.save, "user name allowed to be nil"
+  end
+
+  test "user email never nil" do
+  	@user.email = nil
+    refute @user.save, "user email allowed to be nil"
+  end
+
+  test "user phone never less than 10 digits" do
+    [1,-3,123456789,999999999,5555555].each do |input|
+      @user.phone = input
+      refute @user.save, "phone number #{input} was allowed"
+    end
+  end
+
+  test "phone strips out all non-digit characters" do
+    ["(555) 555-5555","555.555.5555","+555>555>5555", "my phone number is area code 555, number 555-5555"].each do |input|
+      @user.phone = input
+      assert @user.phone = 5555555555, "phone number #{input} was allowed"
+    end
+  end
 end
