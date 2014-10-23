a64b58a5fefab7a67db0d86198e9d6c94f83b6b6
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-23 00:54:25 -0400

    added test to make sure all times are correct

--- test/models/count_allocator_test.rb
+++ test/models/count_allocator_test.rb
@@ -7,4 +7,14 @@ class CountAllocatorTest < ActiveSupport::TestCase
     refute_nil @reminder
     refute_nil @allocator 
   end
+
+  test "allocator assigns times correctly" do
+    @allocator.allocate!
+    all_doses = @reminder.enumerate_doses
+    next_dose = Time.zone.parse('2014-10-1 08:00') 
+    all_doses.count.times do |i|
+      assert_equal all_doses[i].time, next_dose, "#{@reminder.next_dose} is not equal to #{next_dose}"
+      next_dose += 8.hours
+    end
+  end
 end
