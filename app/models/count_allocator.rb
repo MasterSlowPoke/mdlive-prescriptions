3bc00cbca7cef75ffb7e2bb802e1344fa67b469f
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-23 00:13:32 -0400

    Added CountAllocator, handles setting counts on ReminderRules

---
+++ app/models/count_allocator.rb
@@ -0,0 +1,43 @@
+class CountAllocator
+  def initialize(reminder)
+    @reminder = reminder
+  end
+
+  def allocate!
+    return true if @reminder.reminder_rules.empty?
+
+    rule_data = {}
+    @reminder.reminder_rules.each do |rr|
+      rr.schedule = rr.make_schedule(nil, @reminder.start)
+
+      if (rr.schedule.first)
+        rule_data[rr.id] = OpenStruct.new
+        rule_data[rr.id].rule = rr
+        rule_data[rr.id].occurences = 0
+        rule_data[rr.id].next = rr.schedule.first
+      end
+    end
+
+    @reminder.doses.times do
+      # find the time and id of the first rule in rule data
+      next_id = rule_data.keys.first
+      next_time = rule_data[next_id].next
+
+      # test each rule to see which is the earliest
+      rule_data.each do |id, data|
+        if data.next < next_time
+          next_id = id
+          next_time = data.next
+        end
+      end
+
+      # increase the count of the next rule, then find the next time that rule occurs
+      rule_data[next_id].occurences += 1
+      rule_data[next_id].next = rule_data[next_id].rule.schedule.next_occurrence(next_time)
+    end
+
+    @reminder.reminder_rules.each do |rr|
+      rr.update_column(:schedule, rr.make_schedule(rule_data[rr.id].occurences).to_yaml)
+    end
+  end
+end
