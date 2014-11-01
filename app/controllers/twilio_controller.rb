50df21a580a23b8bad51bb6176458986bd93b117
Craig Sniffen <craigsniffen@craigsniffen.com>
2014-10-31 21:32:24 -0400

    testing session varaibles

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -4,6 +4,9 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def text
+    session["counter"] ||= 0
+    sms_count = session["counter"]
+
     sender = params[:From]
     friends = {
       "+18132404479" => "Craig Sniffen"
@@ -11,8 +14,10 @@ class TwilioController < ApplicationController
     name = friends[sender] || "Internet Person"
 
     twiml = Twilio::TwiML::Response.new do |r|
-      r.Message "Hello, #{name}. You just texted me!"
+      r.Message "Hello, #{name}. You just texted me #{sms_count + 1} times!"
     end
+
+    session["counter"] += 1
 
     render xml: twiml.text
   end
